282b4413b7b20ab11af78a1381f0decc1b155403
AndresR <job.devcode@gmail.com>
2019-10-16 17:05:08 -0500

    Cambios en nomenclatura y adiciones modulos

---
+++ PyCharm_Proyectos/inicial_aprendizaje/base_python/30_modulos_1.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# Modulo Main para ejemplo creacion de modulos
+from modulos_symbtable1 import funcion_modular
+
+print("INICIA MODULO MAIN: ")
+funcion_modular("Estos seran", "Los argumentos")
+print("FINALIZA MODULO MAIN: ")
---
+++ PyCharm_Proyectos/inicial_aprendizaje/base_python/modulos_symbtable1.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Funciones y variables para importar en otro modulo "archive"
+
+nombre = "Modulo Personalizado"
+contador = 0
+
+print("Modulo ejecutado")
+
+
+def funcion_modular(*args):
+    print(args)
