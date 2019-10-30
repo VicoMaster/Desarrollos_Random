1d8936d83af19616ce12ccd4b47bfe8a1dc62123
AndresR <job.devcode@gmail.com>
2019-10-30 18:11:05 -0500

    Encapsulamiento de metodos

---
+++ PyCharm_Proyectos/inicial_aprendizaje/poo/encapsulamiento/metodo_estatico.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+Sirva para Poder acceder a los mismos desde cualquier instancia o a travez del objeto clase
+Es otra manera de agrupar metodos
+
+Funcion builtins: staticmethod()
+
+func = staticmethod(func)
+
+decorator: @staticmethod
+
+
+"""
