a8bbcddacad077778159500fc236424123897d40
Andres Rivera <job.devcode@gmail.com>
2021-06-24 00:10:17 -0500

    Nueva version de programa para limpiar temporales, con linea de chrome cache

---
+++ CleanTemporales_V2/src/modelo/Comando_CMD.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+/**Esta Clase Busca el Batch indicado y lo ejecuta,
+ * Posteriormente retorna el resultado de la ejecucion
+ **/
+public class Comando_CMD {
+	//Variables Globales para la Clase
+	
+	/**Metodo donde ejecuta el programa BATCH y se recopila el resultado**/
+	public String iniciarOrden(String archivo) {
+		System.out.println("Ingreso al metodo: iniciarOrden");
+		String contenido = "";
+		String linea = null;
+		File ruta_absoluta = new File("recursos\\"+archivo);
+		String orden_batch = ruta_absoluta.getAbsolutePath();
+		System.out.println(orden_batch);
+		try {
+			Process proceso = Runtime.getRuntime().exec(orden_batch);
+			BufferedReader in = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+			while ((linea = in.readLine()) != null) {
+				System.out.println("Traza: SE ESTA QUEDANDO AQUI...");
+				contenido += "\n"+linea.toString();
+			}
+		} catch (Exception e) {
+			contenido += "Error: "+e;
+		}
+		return contenido;
+	}
+	
+	/**Metodo para separar la ruta absoluta del directorio del contenido del resultado Batch**/
+	public String direccion_Ruta(String contenido) {
+		System.out.println("Ingreso al metodo: direccion_Ruta");
+		System.out.println("LOG: RECUERDE QUE ESTE PROGRAMA DEBE ESTAR EN LA UNIDAD C: ");
+		String [] partes = contenido.split("C:");
+		String usuario = "C:"+partes[1];
+		partes = usuario.split("/");
+		usuario = partes[0];
+		int tamanno_usuario = usuario.length();
+		usuario = usuario.substring(0, (tamanno_usuario - 3));
+		String[] usuario_rescue = usuario.split("\\\\"); //Escape de caracter \
+		usuario = usuario_rescue[0]+"\\"+usuario_rescue[1]+"\\"+usuario_rescue[2];
+		return usuario;
+	}
+}
